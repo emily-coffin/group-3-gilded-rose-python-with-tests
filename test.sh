23e1507fb9cd6b99d62c2f734e2699da4e4aaa75
Emily Coffin <emily.coffin@pediatrix.com>
2023-05-26 10:20:23 -0500

    Migrates to poetry

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-nosetests --cover-branches --with-coverage --rednose --with-watch --cover-erase --cover-html
+echo "Installing Python dependencies..."
+poetry install
+
+echo "Running tests..."
+poetry run pytest -s -vv \
+    --cov="." \
+    --cov-report=term-missing \
+    --cov-report=xml \
+    --cov-fail-under=90 \
+    --junit-xml="./unit_test_results/test_results.xml"
