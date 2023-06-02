24fb4eac162b29ac37c85c24fb9e37be47ae64b8
SMorales <shauna_flowersmorales@mednax.com>
2023-06-02 11:00:17 -0400

    Replaces pylint with ruff

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ analyze_code:
 	./bin/analyze_code.sh
 format:
 	poetry run black .
+	poetry run ruff --fix .
 unit_tests:
 	bin/test.sh
--- bin/analyze_code.sh
+++ bin/analyze_code.sh
@@ -2,7 +2,7 @@ echo "Installing Python dependencies..."
 poetry install
 
 echo "Running pylint..."
-poetry run pylint .
+poetry run ruff check .
 
 echo "Checking code format..."
 poetry run black --check .
