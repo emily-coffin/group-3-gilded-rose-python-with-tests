39331151a3a9022e63b124f358d40ee8c4c70ac3
Chris Caragianis <chris_caragianis@mednax.com>
2023-06-09 10:46:47 -0400

    declassify GildedRose

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODOs
+
+- Get rid of GildedRose class
+- Pull out similar value adjustments into private functions
+- What to do about nested ifs?
+- item quality is mutated, make immutable?
+
--- tests/gilded_rose_test.py
+++ tests/gilded_rose_test.py
@@ -1,9 +1,7 @@
 from src.item import Item
-from src.gilded_rose import GildedRose
+from src.gilded_rose import update_quality
 
 from unittest import TestCase, skip
-
-gilded_rose = GildedRose()
 
 
 class GildedRoseTest(TestCase):
@@ -12,7 +10,7 @@ class GildedRoseTest(TestCase):
 
     def test_regular_items_decrease_by_one(self):
         self.items.append(Item("+5 Dexterity Vest", 10, 20))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = {"sell_in": 9, "quality": 19}
         item = self.items[0]
         self.assertEqual(item.quality, expected["quality"])
@@ -21,7 +19,7 @@ class GildedRoseTest(TestCase):
     def test_quality_goes_up_for_improving_products(self):
         self.items.append(Item("Aged Brie", 20, 30))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 20, 30))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = [
             {"sell_in": 19, "quality": 31},
             {"sell_in": 19, "quality": 31},
@@ -37,7 +35,7 @@ class GildedRoseTest(TestCase):
     ):
         self.items.append(Item("Aged Brie", 10, 34))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 8, 30))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = [
             {"sell_in": 9, "quality": 36},
             {"sell_in": 7, "quality": 32},
@@ -53,7 +51,7 @@ class GildedRoseTest(TestCase):
     ):
         self.items.append(Item("Aged Brie", 4, 11))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 15))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = [
             {"sell_in": 3, "quality": 14},
             {"sell_in": 4, "quality": 18},
@@ -67,7 +65,7 @@ class GildedRoseTest(TestCase):
     def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(self):
         self.items.append(Item("+5 Dexterity Vest", 0, 20))
         self.items.append(Item("Conjured Mana Cake", 0, 6))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = [
             {"sell_in": -1, "quality": 18},
             {"sell_in": -1, "quality": 4},
@@ -81,7 +79,7 @@ class GildedRoseTest(TestCase):
     def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
         self.items.append(Item("Aged Brie", 0, 20))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = [
             {"sell_in": -1, "quality": 0},
             {"sell_in": -1, "quality": 0},
@@ -94,7 +92,7 @@ class GildedRoseTest(TestCase):
 
     def test_sulfuras_the_immutable(self):
         self.items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = {"sell_in": 0, "quality": 80}
         item = self.items[0]
         self.assertEqual(item.quality, expected["quality"])
@@ -102,7 +100,7 @@ class GildedRoseTest(TestCase):
 
     def test_quality_does_not_increase_past_50(self):
         self.items.append(Item("Aged Brie", 4, 49))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = {"sell_in": 3, "quality": 50}
         item = self.items[0]
         self.assertEqual(item.quality, expected["quality"])
@@ -111,7 +109,7 @@ class GildedRoseTest(TestCase):
     @skip
     def test_conjured_items_decrease_in_quality_twice_as_fast(self):
         self.items.append(Item("Conjured Mana Cake", 3, 6))
-        gilded_rose.update_quality(self.items)
+        update_quality(self.items)
         expected = {"sell_in": 2, "quality": 2}
         item = self.items[0]
         self.assertEqual(item.quality, expected["quality"])
