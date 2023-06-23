438bfdf0badecb0aafdd1f39e1d022c3af7f812a
Chris Caragianis <chris_caragianis@mednax.com>
2023-06-23 11:22:23 -0400

    back to square one, with different app code

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -17,9 +17,14 @@ def _item_quality(original_item):
 
 
 def _new_quality(item):
+    new_quality = item.quality
+    if item.name == "Sulfuras, Hand of Ragnaros":
+        return 80
     if item.name in IMPROVING_PRODUCTS:
-        return _new_quality_improving(item)
-    return item.quality - 1
+        new_quality = _new_quality_improving(item)
+    else:
+        new_quality = _new_quality_ordinary(item)
+    return _normalize_quality(new_quality)
 
 
 def _new_quality_improving(item):
@@ -27,4 +32,20 @@ def _new_quality_improving(item):
         return item.quality + 1
     if item.sell_in > 5:
         return item.quality + 2
+    if item.sell_in == 0:
+        return 0
     return item.quality + 3
+
+
+def _new_quality_ordinary(item):
+    if item.sell_in <= 0:
+        return item.quality - 2
+    return item.quality - 1
+
+
+def _normalize_quality(quality):
+    if quality > 50:
+        return 50
+    if quality < 0:
+        return 0
+    return quality
--- tests/gilded_rose_test.py
+++ tests/gilded_rose_test.py
@@ -37,44 +37,7 @@ def test_quality_goes_up_for_improving_products(days_left, quality_change):
         assert new_item.quality == items[index].quality + quality_change
 
 
-@skip
-def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left():
-    items = []
-    items.append(Item("Aged Brie", 10, 34))
-    items.append(Item("Backstage passes to a TAFKAL80ETC concert", 8, 30))
-
-    items = update_quality(items)
-
-    expected = [
-        {"sell_in": 9, "quality": 36},
-        {"sell_in": 7, "quality": 32},
-    ]
-    for index, expectation in enumerate(expected):
-        item = items[index]
-        assert item.quality == expectation["quality"]
-        assert item.sell_in == expectation["sell_in"]
-
-
-@skip
-def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left():
-    items = []
-    items.append(Item("Aged Brie", 4, 11))
-    items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 15))
-
-    items = update_quality(items)
-
-    expected = [
-        {"sell_in": 3, "quality": 14},
-        {"sell_in": 4, "quality": 18},
-    ]
-    for index, expectation in enumerate(expected):
-        item = items[index]
-        assert item.quality == expectation["quality"]
-        assert item.sell_in == expectation["sell_in"]
-
-
-@skip
-def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by():
+def test_quality_decrease_twice_as_fast_after_sell_by():
     items = []
     items.append(Item("+5 Dexterity Vest", 0, 20))
     items.append(Item("Conjured Mana Cake", 0, 6))
@@ -91,48 +54,45 @@ def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by():
         assert item.sell_in == expectation["sell_in"]
 
 
-@skip
 def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by():
     items = []
     items.append(Item("Aged Brie", 0, 20))
-    items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
+    items.append(Item("Backstage passes", 0, 20))
 
     items = update_quality(items)
 
-    expected = [
-        {"sell_in": -1, "quality": 0},
-        {"sell_in": -1, "quality": 0},
-    ]
-    for index, expectation in enumerate(expected):
-        item = items[index]
-        assert item.quality == expectation["quality"]
-        assert item.sell_in == expectation["sell_in"]
+    for item in items:
+        assert item.quality == 0
 
 
-@skip
 def test_sulfuras_the_immutable():
     items = []
     items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
 
     items = update_quality(items)
 
-    expected = {"sell_in": 0, "quality": 80}
     item = items[0]
-    assert item.quality == expected["quality"]
-    assert item.sell_in == expected["sell_in"]
+    assert item.quality == 80
 
 
-@skip
 def test_quality_does_not_increase_past_50():
     items = []
     items.append(Item("Aged Brie", 4, 49))
 
     items = update_quality(items)
 
-    expected = {"sell_in": 3, "quality": 50}
     item = items[0]
-    assert item.quality == expected["quality"]
-    assert item.sell_in == expected["sell_in"]
+    assert item.quality == 50
+
+
+def test_quality_does_go_below_zero():
+    items = []
+    items.append(Item("Normal Thing", 4, 0))
+
+    items = update_quality(items)
+
+    item = items[0]
+    assert item.quality == 0
 
 
 @skip
